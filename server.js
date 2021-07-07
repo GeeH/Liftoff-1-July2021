c1a2d72eb0f4d0a144eb438e6d309e0f91606982
Gary Hockin <gary@hock.in>
2021-07-07 10:03:58 +0100

    Starting point

--- server.js
+++ server.js
@@ -6,33 +6,8 @@ const PORT = 3000;
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
-
-
 app.all("/", (req, res) => {
-   res.contentType("xml");
-    res.send(`
-        <Response>
-            <Message>Use promocode LIFTOFFEMEAJULY for $10 free credit or email ghockin@twilio.com for local currency</Message>
-        </Response>
-    `);
-});
-
-app.all("/sms", (req, res) => {
-    const messagingResponse = new twiml.MessagingResponse();
-    const name = req.body.Body;
-    messagingResponse.message(`Hi ${name} you are amazing`);
-
-    res.send(messagingResponse.toString());
-});
-
-app.all("/voice", (req, res) => {
-    res.contentType("xml");
-
-    const voiceResponse = new twiml.VoiceResponse();
-    voiceResponse.say("Space is big, really big.");
-    voiceResponse.play("https://api.twilio.com/cowbell.mp3");
-
-    res.send(voiceResponse.toString());
+   res.send("ahoy.");
 });
 
 app.listen(PORT, () => {
